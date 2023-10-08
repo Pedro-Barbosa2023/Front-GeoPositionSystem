87cbc795eb5db8712dac6a6c3609129af9d06aee
arthurpontespiazzaroli@gmail.com <arthurpontespiazzaroli@gmail.com>
2023-10-08 19:29:17 -0300

    feat: timestamp

--- backend/src/model/Rastreador.js
+++ backend/src/model/Rastreador.js
@@ -4,7 +4,7 @@ const DataSchema = new mongoose.Schema({
     latitude: String,
     longitude: String,
     altura: String,
-})
+},{ timestamps: true } )
 
 const Rastreador = mongoose.model('Rastreador', DataSchema) // falando pro mongoose criar um model usuario atraves dos campos do DataSchema
 module.exports = Rastreador; 
