c7537217ccf72c4f41968b2fe1876598c2321709
arthurpontespiazzaroli@gmail.com <arthurpontespiazzaroli@gmail.com>
2023-10-11 14:11:22 -0300

    theme change

--- Front-End/src/components/layout/Headers.jsx
+++ Front-End/src/components/layout/Headers.jsx
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
-import {
-  Navbar,
-  NavbarBrand,
-} from "reactstrap";
+import { Navbar, NavbarBrand, Button } from "reactstrap";
 
 const Headers = (args) => {
-  //const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    if (theme === false) {
+      document.body.style.backgroundColor = '#696969';
+      setTheme(true);
+    } else {
+      document.body.style.backgroundColor = '#D3D3D3';
+      setTheme(false);
+    }
+  };
 
   return (
     <div>
-      <>
-        <Navbar className="navBar" dark fixed="top" >
-          <NavbarBrand href="/">
-            <img
-              alt="GPS Locator"
-              src="/images/logo.png"
-              style={{
-                height: 40,
-                width: 40,
-              }}
-            />
+      <Navbar className="navBar" dark fixed="top">
+        <NavbarBrand href="/">
+          <img
+            alt="GPS Locator"
+            src="/images/logo.png"
+            style={{
+              height: 40,
+              width: 40,
+            }}
+          />
           GeoPosition System
-          </NavbarBrand>
-        </Navbar>
-      </>
+        </NavbarBrand>
+        <Button outline onClick={toggleTheme}>Tema</Button>
+      </Navbar>
     </div>
   );
 };
+
 export default Headers;
